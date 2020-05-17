aa9a2a593596f840d8bc869609647b7736f6b98c
Vivek Gupta <vivek.gupta@raweng.com>
2020-05-17 13:32:30 +0530

    Initial Commit

---
+++ Heady-Mart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Heady-Mart
+//
+//  Created by Vivek Gupta on 17/05/20.
+//  Copyright © 2020 Vivek Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Heady-MartTests/Heady_MartTests.swift
@@ -0,0 +1,34 @@
+//
+//  Heady_MartTests.swift
+//  Heady-MartTests
+//
+//  Created by Vivek Gupta on 17/05/20.
+//  Copyright © 2020 Vivek Gupta. All rights reserved.
+//
+
+import XCTest
+@testable import Heady_Mart
+
+class Heady_MartTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
